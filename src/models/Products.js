276703451c644bce1ddca2e59bb1cf1349ae7143
QKleo <leorodr4446@gamil.com>
2022-05-24 08:44:53 -0300

    Merge pull request #1 from LucasRD1978/Developer
    Developer

---
+++ src/models/Products.js
@@ -0,0 +1,42 @@
+const {DataTypes} = require('sequelize');
+
+module.exports = (sequelize) =>{
+
+    sequelize.define('products', {
+
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        image1:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        image2:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        image3:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        image4:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        stock: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        }
+    })
+}
