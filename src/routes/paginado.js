a2fa72230c63c7c9bfca2a2f19928b1aa8c2aeac
QKleo <leorodr4446@gamil.com>
2022-05-27 14:32:19 -0300

    actualizacion nro 1

--- src/routes/paginado.js
+++ src/routes/paginado.js
@@ -4,10 +4,10 @@ const { Router } = require('express');
 
 const route = Router()
 
-route.get('/:numeroPagina',(req,res,next)=>{
-    let  estadoArray=[1,2,3,4,5,6,7,8,9,10,11,12,13]
+route.put('/:numeroPagina',(req,res,next)=>{
+    //let  estadoArray=[1,2,3,4,5,6,7,8,9,10,11,12,13]
     const{numeroPagina}=req.params
-   // const{estadoArray}=req.body
+    const{estadoArray}=req.body
     let cantidadEnVista=3
     let fin=cantidadEnVista*numeroPagina
     let mostrar=estadoArray.slice(fin-cantidadEnVista,fin)
