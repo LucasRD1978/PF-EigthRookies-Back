fd890fbe90427909977842898beddccada05097d
QKleo <leorodr4446@gamil.com>
2022-05-27 14:27:43 -0300

    actualizacion nro 1

---
+++ src/routes/paginado.js
@@ -0,0 +1,26 @@
+const {Products, Category} =require('../db.js');
+const axios = require('axios');
+const { Router } = require('express');
+
+const route = Router()
+
+route.get('/:numeroPagina',(req,res,next)=>{
+    let  estadoArray=[1,2,3,4,5,6,7,8,9,10,11,12,13]
+    const{numeroPagina}=req.params
+   // const{estadoArray}=req.body
+    let cantidadEnVista=3
+    let fin=cantidadEnVista*numeroPagina
+    let mostrar=estadoArray.slice(fin-cantidadEnVista,fin)
+    return res.send(mostrar)
+
+
+})
+
+
+
+
+
+
+
+
+module.exports=route
