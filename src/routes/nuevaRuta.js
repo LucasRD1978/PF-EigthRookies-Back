e281b3e7c77fc681659d2c78f3519fcb9d10e723
Lucas Rafael Dominguez <86419120+LucasRD1978@users.noreply.github.com>
2022-06-02 18:12:22 -0300

    Revert "correccion offset"

--- src/routes/nuevaRuta.js
+++ src/routes/nuevaRuta.js
@@ -7,15 +7,15 @@ const route = Router()
 route.put('/:page',(req,res,next)=>{
     //const category=''
     //const price=''
-    console.log(rteq.body)
+    //console.log(rteq.body)
     const{page}=req.params
     const{category,price}=req.body
     if(category&&price){
     Products.findAll({
             where:{price:{ [Op.gt]:[price*1],
                 [Op.lt]:[price*1+5000]}},
-                offset:page*10+1,
-                limit:10,
+                offset:page+6,
+                limit:6,
                 include:[{
                 model:Category,
                 where:{id:category*1}
@@ -26,8 +26,8 @@ route.put('/:page',(req,res,next)=>{
     else if(category&&!price){
         Products.findAll({
             include:[{
-                offset:page*10+1,
-                limit:10,
+                offset:page+6,
+                limit:6,
                 model:Category,
                 where:{id:category*1}
             }]
@@ -37,8 +37,8 @@ route.put('/:page',(req,res,next)=>{
     }
     else if(!category&&price){
         Products.findAll({
-            offset:page*10+1,
-            limit:10,                             
+            offset:page+6,
+            limit:6,                             
             where:{price:{[Op.gt]:[price*1-5000],
                 [Op.lt]:[price*1+5000]}}
         })
@@ -46,8 +46,8 @@ route.put('/:page',(req,res,next)=>{
         .catch(()=>next())
     } 
     Products.findAll({
-        offset:page*10+1,
-        limit:10,                                                                                                                
+        offset:page+6,
+        limit:6,                                                                                                                
     })
     .then((r)=>{return res.status(200).send(r)})
     .catch(()=>next())                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
